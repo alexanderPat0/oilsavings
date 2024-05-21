8aefd3205e2258b6881dff7452930df67850d6ab
alex0rodriguez7fenix <alex0rodriguez7fenix@gmail.com>
2024-05-21 15:54:27 +0000

    Creado gasStationModel

--- lib/models/GasStationModel.dart
+++ lib/models/GasStationModel.dart
@@ -1,95 +1,80 @@
-// class GasStations {
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class GasStations {
+
+  bool? success;
+  List<GasStationData>? data;
+  String? message;
+
+  GasStations({this.success, this.data, this.message});
+
+  GasStations.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    if (json['data'] != null) {
+      data = <GasStationData>[];
+      json['data'].forEach((v) {
+        data!.add(GasStationData.fromJson(v));
+      });
+    }
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['success'] = success;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['message'] = message;
+    return data;
+  }
+}
+
+class GasStationData {
+  int? id;
+  String? company;
+  String? location;
+  int? valoration;
+  Map<String, int>? fuelPrices; // Map for key-value pairs
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  GasStationData({
+    this.id,
+    this.company,
+    this.location,
+    this.valoration,
+    this.fuelPrices,
+    this.createdAt,
+    this.updatedAt,
+  });
 
 
-//     EN CUANTO CONSIGA RECOGER LA INFORMACIÓN CON LA API
-//     TENGO QUE ENCONTRAR QUÉ INFORMACIÓN EXACTA ME VA A DEVOLVER
-//     PARA INTRODUCIRLA AQUÍ Y PODER MOSTRAR LA INFORMACIÓN DE LAS 
-//     GASOLINERAS CERCANAS.
-//   bool? success;
-//   List<UserData>? data;
-//   String? message;
+   GasStationData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    company = json['company'];
+    location = json['location'];
+    valoration = json['valoration'];
+    // Convert fuelPrices from Map<String, dynamic> to Map<String, int>
+    if (json['fuelPrices'] != null) {
+      fuelPrices = Map<String, int>.from(json['fuelPrices']);
+    }
+    createdAt = json['created_at'] != null ? DateTime.parse(json['created_at']) : null;
+    updatedAt = json['updated_at'] != null ? DateTime.parse(json['updated_at']) : null;
+  }
 
-//   GasStations({this.success, this.data, this.message});
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['company'] = company;
+    data['location'] = location;
+    data['valoration'] = valoration;
+    data['fuelPrices'] = fuelPrices;
+    data['created_at'] = createdAt?.toIso8601String();
+    data['updated_at'] = updatedAt?.toIso8601String();
+    return data;
+  }
 
-//   GasStations.fromJson(Map<String, dynamic> json) {
-//     success = json['success'];
-//     if (json['data'] != null) {
-//       data = <UserData>[];
-//       json['data'].forEach((v) {
-//         data!.add(UserData.fromJson(v));
-//       });
-//     }
-//     message = json['message'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = <String, dynamic>{};
-//     data['success'] = success;
-//     if (this.data != null) {
-//       data['data'] = this.data!.map((v) => v.toJson()).toList();
-//     }
-//     data['message'] = message;
-//     return data;
-//   }
-// }
-
-// class GasStationData {
-
- 
-
-
-//   int? id;
-//   String? username;
-//   String? email;
-//   String? mainFuel;
-//   int? emailConfirmed;
-//   DateTime? emailVerifiedAt;
-//   String? password;
-//   int? rememberToken;
-//   int? deleted;
-//   DateTime? createdAt;
-//   DateTime? updatedAt;
-
-//   UserData(
-//       {this.id,
-//       this.username,
-//       this.email,
-//       this.mainFuel,
-//       this.emailConfirmed,
-//       this.emailVerifiedAt,
-//       this.password,
-//       this.rememberToken,
-//       this.deleted,
-//       this.createdAt,
-//       this.updatedAt});
-
-//   UserData.fromJson(Map<String, dynamic> json) {
-//     id = json['id'];
-//     name = json['username'];
-//     email = json['email'];
-//     mainFuel = json['mainFuel'];
-//     emailConfirmed = json['email_confirmed'];
-//     emailVerifiedAt = json['email_verified_at'];
-//     password = json['password'];
-//     rememberToken = json['remember_token'];
-//     deleted = json['deleted'];
-//     createdAt = json['created_at'];
-//     updatedAt = json['updated_at'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = <String, dynamic>{};
-//     data['id'] = id;
-//     data['username'] = username;
-//     data['email'] = email;
-//     data['mainFuel'] = mainFuel;
-//     data['email_confirmed'] = emailConfirmed;
-//     data['email_verified_at'] = emailVerifiedAt;
-//     data['password'] = password;
-//     data['remember_token'] = rememberToken;
-//     data['deleted'] = deleted;
-//     data['created_at'] = createdAt;
-//     data['updated_at'] = updatedAt;
-//     return data;
-//   }
-// }
+}
